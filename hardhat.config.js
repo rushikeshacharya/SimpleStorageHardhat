b85f69f945e683192e1417517e329afdb2ea8cda
Rushikesh Acharya <v-racharya@microsoft.com>
2023-05-29 10:49:47 +0530

    feat: add sepolia url and account

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,8 +12,8 @@ module.exports = {
       url: "http:127.0.0.1:8545"
     },
     sepolia: {
-      url: "https://sepolia.infura.io/v3/<key>"
-      // accounts: [privateKey1, privateKey2, ...];
+      url: "https://sepolia.infura.io/v3/<api-key>",
+      accounts: ["private-key"]
     }
   },
 };
